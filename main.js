07e61da0e54ead4b28a617fbcec13fb7d3f7ca23
sherbek turdiyev <sherbekturdiyev98@gmail.com>
2024-07-11 02:41:52 +0500

    full back

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
 const http = require('http');
 
 const data = [
-    { 'email': 'jim@gmail.com', 'number': '221122' },
-    { 'email': 'jam@gmail.com', 'number': '830347' },
-    { 'email': 'john@gmail.com', 'number': '221122' },
-    { 'email': 'jams@gmail.com', 'number': '349425' },
-    { 'email': 'jams@gmail.com', 'number': '141424' },
-    { 'email': 'jill@gmail.com', 'number': '822287' },
-    { 'email': 'jill@gmail.com', 'number': '822286' }
+    { 'email': 'jim@gmail.com', 'number': '(90)221-41-22' },
+    { 'email': 'jam@gmail.com', 'number': '(91)830-43-47' },
+    { 'email': 'john@gmail.com', 'number': '(99)221-41-22' },
+    { 'email': 'jams@gmail.com', 'number': '(94)349-44-25' },
+    { 'email': 'jams@gmail.com', 'number': '(93)141-44-24' },
+    { 'email': 'jill@gmail.com', 'number': '(99)822-42-87' },
+    { 'email': 'jill@gmail.com', 'number': '(91)822-42-86' }
 ];
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/api/data') {
+const server = http.createServer(async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+    if (req.method === 'GET' && req.url === '/api/data/all') {
         res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(data))
-    } else if(req.method === 'POST' && req.url === 'api/data'){
+        res.end(JSON.stringify(data));
+    } else if(req.method === 'POST' && req.url === '/api/data/search'){
         let body = '';
-        req.on('data',chunk => {
-            body += toString();
+        await req.on('data',chunk => {
+            body += chunk.toString();
         });
-        req.on('end', () => {
+        await req.on('end', async () => {
             const {email, number} = JSON.parse(body);
-            const found = data.filter(item => item.email === email &&
-                item.number === number);
-            if (found.length > 0) {
-                res.writeHead(200, {'Content-Type' : 'application/json'});
-                res.end(JSON.stringify(found));
+            let found;
+            if(number){
+                found = data.filter(item => item.email === email && number ? item.number === number: '');
             }else {
-                res.writeHead(404, {'Content-Type' : 'application/json'});
-                res.end('Not-found');
+                found = data.filter(item => item.email === email);
+            }
+            if (found.length > 0) {
+                await res.writeHead(200, {'Content-Type' : 'application/json'});
+                await res.end(JSON.stringify(found));
+            }else {
+                await res.writeHead(404, {'Content-Type' : 'application/json'});
+                await res.end('Not-found');
             }
         })
     }else {
